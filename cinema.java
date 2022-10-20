5c0332899b578863d19bf279067a319026cc56ab
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-10-20 16:03:03 +0800

    first ver

---
+++ cinema.java
@@ -0,0 +1,71 @@
+import java.util.List;
+
+public class cinema extends cineplex{
+    public enum ClassOfCinema {
+        Platinum, Gold, Silver
+    }
+
+    public enum Layout{
+        small, medium, large
+    }
+
+    //attributes
+    private int cinemaID;
+    private int numberOfSeats;
+    private int seatLeft;
+    private ClassOfCinema classOfCinema;
+    private Layout layout;
+
+    //constructor
+    public cinema(int cinemaID, int numberOfSeats, int seatLeft,ClassOfCinema classOfCinema, Layout layout) {
+        this.cinemaID = cinemaID;
+        this.numberOfSeats = numberOfSeats;
+        this.seatLeft = seatLeft;
+        this.classOfCinema = classOfCinema;
+        this.layout = layout;
+    }
+
+    //getter
+    public int getCinemaID() {
+        return cinemaID;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public int getSeatLeft() {
+        return seatLeft;
+    }
+
+    public ClassOfCinema getClassOfCinema() {
+        return classOfCinema;
+    }
+
+    public Layout getLayout() {
+        return layout;
+    }
+
+    //setter
+
+    public void setCinemaID(int cinemaID) {
+        this.cinemaID = cinemaID;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void setSeatLeft(int seatLeft) {
+        this.seatLeft = seatLeft;
+    }
+
+    public void setClassOfCinema(ClassOfCinema classOfCinema) {
+        this.classOfCinema = classOfCinema;
+    }
+
+    public void setLayout(Layout layout) {
+        this.layout = layout;
+    }
+
+}
---
+++ cineplex.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class cineplex {
+    //input attributes
+    private List<cinema> cinemaList;
+    private List<movie> movieList;
+
+    public static void main(String[] args) {
+        
+    }
+}
