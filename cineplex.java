500a16e7fc6649c024a5fc8f1e446517aac894ad
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-10-20 16:13:32 +0800

    .as

--- cineplex.java
+++ cineplex.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Cineplex {
     //input attributes
     private List<Cinema> cinemaList;
-    private List<movie> movieList;
+    private List<Movie> movieList;
 
     public static void main(String[] args) {
         
