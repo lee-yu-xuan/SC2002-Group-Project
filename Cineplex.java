197f6dd21ea19a69dd3541ed57e2fa2eb45c6e44
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-10-20 16:17:02 +0800

    .

---
+++ Cineplex.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cineplex {
+    //input attributes
+    private List<Cinema> cinemaList;
+    private List<Movie> movieList;
+
+    public static void main(String[] args) {
+        
+    }
+}
