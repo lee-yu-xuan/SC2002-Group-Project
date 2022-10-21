8f96e90e4c7ac46f74cc8321a65e19aaca1713b7
lee-yu-xuan <102302118+lee-yu-xuan@users.noreply.github.com>
2022-10-21 20:20:51 +0800

    Add files via upload

--- Booking.java
+++ Booking.java
@@ -1,16 +1,34 @@
 package SC2002Project;
 
+import java.util.List;
+
 public class Booking
 {
 	private String transactionID;
 	private String name;
-	private String mobileNumber;
+	private String cinema;
+	private String pax;
+	private String seats;
+	private String cost;
 	
-	public Booking(String transactionID, String name, String mobileNumber)
+	public Booking(String transactionID, String name, String cinema, int pax, String seats ,double cost)
 	{
 		this.transactionID = transactionID;
 		this.name = name;
-		this.mobileNumber = mobileNumber;
+		this.cinema = cinema;
+		this.pax = String.valueOf(pax);
+		this.seats = seats;
+		this.cost = String.valueOf(cost);
 		//File IO into database
+		String path = "/BookingHistory/" + this.name;
+		List<String[]> bookingHistory = File_IO.readFile(path);
+		int column = bookingHistory.size();
+		bookingHistory.get(column)[0] = this.transactionID;
+		bookingHistory.get(column)[1] = this.name;
+		bookingHistory.get(column)[2] = this.cinema;
+		bookingHistory.get(column)[3] = this.pax;
+		bookingHistory.get(column)[4] = this.seats;
+		bookingHistory.get(column)[5] = this.cost;
+		File_IO.writeFile(bookingHistory,path);
 	}
 }
