b383bf1d5879cf059ca35d31f032d2c3f1128461
Lim Yao Xian <102975373+LimYaoXian@users.noreply.github.com>
2022-10-21 20:38:46 +0800

    .

--- Staff.java
+++ Staff.java
@@ -1,9 +1,11 @@
-package SC2002Project;
+package OODP;
 
 import java.util.List;
+import java.util.*;
 
 public class Staff extends constructUser
 { 
+	Scanner scan = new Scanner(System.in);
 	MovieController MC = new MovieController();
 	 public Staff(String name, String username, String password)
 	 {
@@ -14,16 +16,107 @@ public class Staff extends constructUser
 	 }
 	 public void addMovie()
 	{
+		String MovieName="";
+		String Enum="Coming Soon";
+		String Synopsis = "";
+		String Director = "";
+		String Cast = "";
+		String Rating = "NA";
+		String reviews = "";
+		String time="";
+		do {
+		 System.out.println("Enter the name of the Movie: ");
+		 try {
+			 MovieName = scan.nextLine();
+		 }
+		 catch(NumberFormatException nfe) {
+			 System.out.println("Please enter in string!");
+		 }
+		}while (MovieName.isEmpty());
+		
 		 
-		//MC.add(String MovieName, Enum status, String Synopsis,String director,String Cast,double rating, String reviews);
+		do {
+			System.out.println("Enter the Synopsis: ");
+			 try {
+				 Synopsis = scan.nextLine();
+			 }
+			 catch(NumberFormatException nfe) {
+				 System.out.println("Please enter in string!");
+			 }
+			}while (Synopsis.isEmpty());
+		 
+		do {
+			System.out.println("Enter the name of the director: ");
+			 try {
+				 Director = scan.nextLine();
+			 }
+			 catch(NumberFormatException nfe) {
+				 System.out.println("Please enter in string!");
+			 }
+			}while (Director.isEmpty());
+		 
+		do {
+			System.out.println("Enter the Cast: ");
+			 try {
+				 Cast = scan.nextLine();
+			 }
+			 catch(NumberFormatException nfe) {
+				 System.out.println("Please enter in string!");
+			 }
+			}while (Director.isEmpty());
+		 
+		
+		do {
+			System.out.println("Enter the reviews: ");
+			 try {
+				 reviews = scan.nextLine();
+			 }
+			 catch(NumberFormatException nfe) {
+				 System.out.println("Please enter in string!");
+			 }
+			}while (reviews.isEmpty());
+		
+		do {
+			System.out.println("Enter the showing time: ");
+			 try {
+				 time = scan.nextLine();
+			 }
+			 catch(NumberFormatException nfe) {
+				 System.out.println("Please enter in string!");
+			 }
+			}while (time.isEmpty());
+		 
+			//MC.add(String MovieName, Enum status, String Synopsis,String director,String Cast,double rating, String reviews); 
+		 
+		
+		 
 	}
 	 
 	 public void removeMovie()
 	 {
-		 //MC.remove(String MovieName);
+		 //MC.changeState();
 	 }
-	 public void checkRating()
+	 public void topFiveMovies()
 	 {
-		 //MC.getRating(String Moviename);
+		 int choice=0;
+		 do {
+			 System.out.println("Please choose one of the options");
+			 System.out.println("1) Ranked by ticketSales");
+			 System.out.println("2) Ranked by reviewers rating");
+		 
+			 try {
+				 choice = scan.nextInt();
+			 }
+			 catch(NumberFormatException nfe) {
+				 System.out.println("Please enter a number!");
+			 }
+		 }while(choice>2 || choice<1);
+		 
+		 if(choice ==1) {
+			 //MC.showMovieByTicketSales();
+		 }
+		 if(choice ==2) {
+			 //MC.showMovieByRating();
+		 }
 	 }
 }
