f55ce7cb41f5aec3bcf94371fb68e08ef79f32b1
rawang45 <csbhlim@gmail.com>
2022-10-30 16:37:48 +0800

    fix: Change File_IO seperator to be \t (tab)

--- MovieManager.java
+++ MovieManager.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieManager {
-    
+    private static List<Movie> movieList;
+    private static List<String[]> movieCSV;
+
     public static void addMovie(String moviename, String Synopsis,String director,String Cast,String time){
         
     }
@@ -12,5 +17,24 @@ public class MovieManager {
 
     public static void viewRating(){
         
+    }
+
+    public static void addMovie(List<String[]> temp){
+        File_IO.writeFile(temp, "testMovieList");
+    }
+
+    public static void load(){
+        movieCSV = File_IO.readFile("movieList");
+        System.out.println(movieCSV.size());
+        System.out.println();
+        for(int i =0;i<movieCSV.size();i++){
+            System.out.println(movieCSV.get(i).length);
+            for(int j = 0;j<movieCSV.get(i).length;j++){
+                System.out.println(movieCSV.get(i)[j]);
+                if(movieCSV.get(i)[0].charAt(0) == '\"'){
+                    movieCSV.get(i)[0].length();
+                }
+            }
+        }
     }
 }
