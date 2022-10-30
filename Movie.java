80384851e2b3fb1c2214028e89be2cf698a256e8
rawang45 <csbhlim@gmail.com>
2022-10-30 23:08:11 +0800

    feat: Movie.java attribute added

--- Movie.java
+++ Movie.java
@@ -1,15 +1,52 @@
+import java.util.List;
+
 /**
  * Movie
  */
-public class Movie {
+public class Movie implements MovieInterface{
     private String movieTitle;
-    private enum ShowingStatus {COMING_SOON, PREVIEW, NOW_SHOWING};
+    private ShowingStatus showingStatus;
     private String synopsis;
     private String director;
     private String cast;
-    private Review review; 
+    private Review review;
+    private String type;
     private String movieID;
-    
-    public Movie(){
+
+    public Movie(String movieTitle, String synopsis, String director, String cast, String type, String movieID){
+        this.movieTitle = movieTitle;
+        this.showingStatus = ShowingStatus.COMING_SOON;
+        this.director = director;
+        this.synopsis = synopsis;
+        this.cast = cast;
+        this.review = null;
+        this.type = type;
+        this.movieID = movieID;
     }
+
+    public void setTitle(String movieTitle){
+        this.movieTitle = movieTitle;
+    }
+
+    public void setStatus(ShowingStatus showingStatus){
+        this.showingStatus = showingStatus;
+    }
+
+    public void setSynopsis(String synopsis){
+        this.synopsis = synopsis;
+    }
+
+    public void setDirector(String director){
+        this.director = director;
+    }
+
+    public void setCast(String cast){
+        this.cast = cast;
+    }
+
+    public Review getReview(){
+        return this.review;
+    }
+
+    public 
 }
