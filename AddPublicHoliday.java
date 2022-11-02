d0579d771817a736ec8e0322ded490f4536250a1
Lester0142 <karjunlester@gmail.com>
2022-11-02 21:35:34 +0800

    systemconfig

---
+++ AddPublicHoliday.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class AddPublicHoliday{
+
+    List<String[]> days = new ArrayList<>();
+    // constructor
+    public AddPublicHoliday(){}
+
+    // in yyyy-MM-dd;
+    public void Add(String date){
+        String start = date + " 00:00";
+        String end = date + " 23:59";
+        String[] day = new String[2];
+        System.out.println(start + "\\" + end);
+        day[0] = start;
+        day[1] = end;
+        days.add(day);
+    }
+
+    public void updateCSV(){
+        File_IO.writeFile(days, "PublicHoliday");
+    }
+
+    // public static void main (String[] args){
+    //     AddPublicHoliday test_add = new AddPublicHoliday();
+    //     test_add.Add("2022-12-25");
+    //     test_add.Add("2022-12-31");
+    //     test_add.updateCSV();
+    // }
+
+}
