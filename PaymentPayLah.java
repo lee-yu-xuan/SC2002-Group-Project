7f3542fa697a15c4f8a716deee1667a842f033c2
rawang45 <csbhlim@gmail.com>
2022-11-02 20:14:21 +0800

    feat: Payment feature done. Debug needed

---
+++ PaymentPayLah.java
@@ -0,0 +1,34 @@
+public class PaymentPayLah implements PaymentMethodInterface{
+    private int validationCount;
+
+    public int pay() {
+        validationCount = 0;
+        System.out.println("Enter your bank card details:");
+
+        while(validationCount < 2){ //User have 2 chances to payByPayLah
+            System.out.println("Please scan the QR code below");
+            System.out.println();
+            System.out.println("==================================");
+            System.out.println("QR CODE ARE SUPPOSED TO SHOW HERE");
+            System.out.println("==================================");
+            System.out.println();
+            System.out.println("Press Enter after you have made the transaction...");
+
+            if(validation() == 1){
+                //payment successful
+                return 1;
+            }else{
+                validationCount++;
+                System.out.println("Payment Unsuccessful. Try again");
+            }
+        }
+        //payment unsuccessful
+        return -1;
+    }
+    
+    public int validation(){
+        //go to API and check if the payment go through
+        //We assume it is always successful here.
+        return 1;
+    }
+}
