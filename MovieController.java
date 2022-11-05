d8dd7b83ae50b04f48b64baddee56b237815584f
rawang45 <csbhlim@gmail.com>
2022-11-05 12:42:10 +0800

    feat: showAvailableMovieBySales

--- MovieController.java
+++ MovieController.java
@@ -47,5 +47,47 @@ public class MovieController {
         return movieList;
     }
 
-    
+    public static List<Movie> showAvailableMovieByRating(){
+        List<Movie> movieList = MovieListing.getAvailableMovieList();
+        List<String> movieID = new ArrayList<String>();
+        for(int i = 0;i<movieList.size();i++){
+            movieID.add(movieList.get(i).getMovieID());
+        }
+
+        //insertion sort
+        for(int i = 1;i<movieList.size();i++){
+            for(int j = i;j> 0;j--){
+                double curRating = ReviewList.getAvgRating(movieID.get(j));
+                double preRating = ReviewList.getAvgRating(movieID.get(j-1));
+                if(curRating < preRating){
+                    Collections.swap(movieList,j,j-1);
+                }
+                else break;
+            }
+        }
+        
+        return movieList;
+    }
+
+    public static List<Movie> showAvailableMovieByTicketSales(){
+        List<Movie> movieList = MovieListing.getAvailableMovieList();
+        List<String> movieID = new ArrayList<String>();
+        for(int i = 0;i<movieList.size();i++){
+            movieID.add(movieList.get(i).getMovieID());
+        }
+
+        //insertion sort
+        for(int i = 1;i<movieList.size();i++){
+            for(int j = i;j> 0;j--){
+                double curRating = SalesManager.getSalesByID(movieID.get(j));
+                double preRating = SalesManager.getSalesByID(movieID.get(j-1));
+                if(curRating < preRating){
+                    Collections.swap(movieList,j,j-1);
+                }
+                else break;
+            }
+        }
+        
+        return movieList;
+    }
 }
