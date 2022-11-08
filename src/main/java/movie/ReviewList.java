c8a4cbc0627ee06b709932e60149ab512e853661
lee-yu-xuan <102302118+lee-yu-xuan@users.noreply.github.com>
2022-11-09 01:06:55 +0800

    Add files via upload
    javadoc

--- src/main/java/movie/ReviewList.java
+++ src/main/java/movie/ReviewList.java
@@ -5,10 +5,20 @@ import java.util.Collections;
 import java.util.List;
 import src.main.java.helper.*;
 
+/**
+ * A ReviewList Class.
+ * @author Luke Yong Jian
+ * @version 1.0
+ * @since 2022-11-01
+ */
+
 public class ReviewList {
     private static List<Review> reviewList;
     private static List<String[]> reviewCSV;
 
+    /*
+     * sort the list after inserting new review
+     */
     private static void sortAfterAppend()
 	{
         int position = reviewList.size() - 1;
@@ -19,8 +29,12 @@ public class ReviewList {
             position--;
         }
 	}
-
-    public static double getAvgRating(String movieID){
+    /**
+     * get the average rating of a certain movie
+     * @param movieID id of movie
+     * @return average rating of a certain movie
+     */
+    public static double getAvgRating(String movieID) {
         double total = 0;
         int ratingCount = 0;
         for(int i = 0;i<reviewList.size();i++){
@@ -37,7 +51,11 @@ public class ReviewList {
         //System.out.println("MovieID not found!");
         return -1.0;
     }
-
+    /**
+     * get the list of review of a certain movie
+     * @param movieID id of movie
+     * @return the list of review of a certain movie
+     */
     public static List<Review> getReviewByID(String movieID){
         List<Review> returnMovieList = new ArrayList<Review>();
         for(int i = 0;i<reviewList.size();i++){
@@ -51,6 +69,12 @@ public class ReviewList {
         return returnMovieList;
     }
 
+    /**
+     * @param movieID id of a movie
+     * @param review review of a movie
+     * @param rating rating of a movie
+     * inserts a new review and rating into a certain movie
+     */
     public static void addRatingReview(String movieID, String review, int rating){
         if(reviewList == null){
             reviewList = new ArrayList<Review>();
@@ -58,15 +82,23 @@ public class ReviewList {
         reviewList.add(new Review(movieID, rating, review));
         sortAfterAppend();
     }
-
+    /**
+     * delete a specific rating
+     * @param index index of the rating
+     */
     public static void delRatingByIndex(int index){
         reviewList.remove(index);
     }
 
+    /**
+     * @return a list of reviews
+     */
     public static List<Review> getReview(){
         return reviewList;
     }
-
+    /**
+     * Load the content of review list CSV into the system
+     */ 
     public static void load(){
         if(reviewList == null){
             reviewList = new ArrayList<Review>();
@@ -81,7 +113,9 @@ public class ReviewList {
         
     }  
 
-
+    /**
+     * Save the content of the system into the review list CSV
+     */
     public static void save(){
         List<String[]> writeBackReview = new ArrayList<String[]>();
 
