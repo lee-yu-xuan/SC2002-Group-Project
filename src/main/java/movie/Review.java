a243b41654efbe8c36246667cc66c7d881c3ccbd
lee-yu-xuan <102302118+lee-yu-xuan@users.noreply.github.com>
2022-11-09 00:58:39 +0800

    Add files via upload
    javadoc update

--- src/main/java/movie/Review.java
+++ src/main/java/movie/Review.java
@@ -1,10 +1,19 @@
 package src.main.java.movie;
 
+/**
+ * A MovieListing Class.
+ * @author Luke Yong Jian
+ * @version 1.0
+ * @since 2022-11-01
+ */
+
 public class Review implements Comparable{
     private int rating;
     private String review;
     private String movieID;
-
+     /**
+     * compareTo method for Review Comparable.
+     */
     public int compareTo(Object o){
         if(o == null){
             return -1;
@@ -22,38 +31,64 @@ public class Review implements Comparable{
             }
         }
     }
+
+    /**
+     * Constructor of Review class.
+     * @param movieID String movie ID
+     */
     public Review(String movieID){
         this.movieID = movieID;
         rating = 0;
         review = "";
     }
 
+     /**
+     * Constructor of Review class.
+     * @param movieID String movie ID
+     * @param rating rating of movie
+     * @param review review of movie
+     */
     public Review(String movieID, int rating, String review){
         this.movieID = movieID;
         this.rating = rating;
         this.review = review;
     }
-
+    /**
+     * @return rating of the movie
+     */
     public int getRating() {
         return rating;
     }
-
+    /**
+     * @return review of the movie
+     */
     public String getReview() {
         return review;
     }
-    
+    /**
+     * @return id of the movie
+     */
     public String getMovieID() {
         return movieID;
     }
-
+    /**
+     * set the review of the movie
+     * @param review review of the movie
+     */
     public void setReview(String review) {
         this.review = review;
     }
-
+    /**
+     * set the rating of the movie
+     * @param rating review of the movie
+     */
     public void setRating(int rating) {
         this.rating = rating;
     }
-
+    /**
+     * set the id of the movie
+     * @param movieID id of the movie
+     */
     public void setMovieID(String movieID) {
         this.movieID = movieID;
     }
