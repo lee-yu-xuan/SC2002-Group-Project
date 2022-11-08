93274ac9d260329636d1378ba2c5c9df484be7f6
lee-yu-xuan <102302118+lee-yu-xuan@users.noreply.github.com>
2022-11-09 00:28:25 +0800

    Add files via upload
    javadoc for movie

--- src/main/java/movie/Movie.java
+++ src/main/java/movie/Movie.java
@@ -3,7 +3,10 @@ package src.main.java.movie;
 import src.main.java.enums.ShowingStatus;
 
 /**
- * Movie
+ * A Movie Class.
+ * @author Yong Jian
+ * @version 1.0
+ * @since 2022-11-01
  */
 public class Movie{
     private String movieTitle;
@@ -15,6 +18,16 @@ public class Movie{
     private String rating;
     private String movieID;
 
+    /**
+     * Constructor of Movie class.
+     * @param movieTitle String movie title.
+     * @param synopsis String synopsis of movie.
+     * @param type String genre of movie.
+     * @param director String name of director.
+     * @param cast String name of cast.
+     * @param rating String rating of movie.
+     * @param movieID String movie ID
+     */
     public Movie(String movieTitle, String synopsis, String type,String director, String cast, String rating,String movieID){
         this.movieTitle = movieTitle;
         //this.showingStatus = ShowingStatus.COMING_SOON;
@@ -25,6 +38,17 @@ public class Movie{
         this.rating = rating;
         this.movieID = movieID;
     }
+     /**
+     * Constructor of Movie class.
+     * @param movieTitle String movie title.
+     * @param showingStatus ShowingStatus: movie status
+     * @param synopsis String synopsis of movie.
+     * @param type String genre of movie.
+     * @param director String name of director.
+     * @param cast String name of cast.
+     * @param rating String rating of movie.
+     * @param movieID String movie ID
+     */
     public Movie(String movieTitle, ShowingStatus showingStatus, String synopsis, String type,String director, String cast, String rating,String movieID){
         this.movieTitle = movieTitle;
         this.showingStatus = showingStatus;
@@ -35,66 +59,109 @@ public class Movie{
         this.rating = rating;
         this.movieID = movieID;
     }
-
+    /**
+     * Set the title of a movie.
+     * @param movieTitle String of movieTitle.
+     */
     public void setTitle(String movieTitle){
         this.movieTitle = movieTitle;
     }
-
+    /**
+     * Set the status of a movie.
+     * @param showingStatus Enum of movie showing status.
+     */
     public void setStatus(ShowingStatus showingStatus){
         this.showingStatus = showingStatus;
     }
-
+    /**
+     * Set the synposis of a movie.
+     * @param synopsis String of movie synopsis.
+     */
     public void setSynopsis(String synopsis){
         this.synopsis = synopsis;
     }
-
+    /**
+     * Set the director of a movie.
+     * @param director String of movie director.
+     */
     public void setDirector(String director){
         this.director = director;
     }
-
+    /**
+     * Set the cast of a movie.
+     * @param cast String of movie cast.
+     */
     public void setCast(String cast){
         this.cast = cast;
     }
-
+    /**
+     * Set the rating of a movie.
+     * @param rating String of movie ratings.
+     */
     public void setRating(String rating){
         this.rating = rating;
     }
-    
+    /**
+     * Set the genre of a movie.
+     * @param type String of movie genre.
+     */    
     public void setType(String type) {
     	this.type=type;
     }
-
+    /**
+     * Get Movie Title function.
+     * @return the movie title.
+     */ 
     public String getMovieTitle(){
         return movieTitle;
     }
-    
+    /**
+     * Get Movie Showing Status function.
+     * @return the showing status.
+     */    
     public ShowingStatus getShowingStatus() {
         return showingStatus;
     }
-
+    /**
+     * Get Movie synopsis function.
+     * @return the synopsis.
+     */ 
     public String getSynopsis() {
         return synopsis;
     }
-
+    /**
+     * Get Movie genre function.
+     * @return the genre.
+     */ 
     public String getType() {
         return type;
     }
-
+    /**
+     * Get Movie Director function.
+     * @return the director.
+     */ 
     public String getDirector() {
         return director;
     }
-
+    /**
+     * Get Movie cast function.
+     * @return the cast.
+     */ 
     public String getCast() {
         return cast;
     }
-
+    /**
+     * Get Movie rating function.
+     * @return the rating.
+     */ 
     public String getRating() {
         return rating;
     }
-
+    /**
+     * Get Movie ID function.
+     * @return the movie ID.
+     */ 
     public String getMovieID(){
         return movieID;
     }
-    
-
 }
