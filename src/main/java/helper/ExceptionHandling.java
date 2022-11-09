8949249758da88448306f10e4daa4ecdd7eff518
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-11-09 19:29:24 +0800

    comments

--- src/main/java/helper/ExceptionHandling.java
+++ src/main/java/helper/ExceptionHandling.java
@@ -1,4 +1,5 @@
 package src.main.java.helper;
+
 import java.time.LocalDateTime;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -8,6 +9,7 @@ import src.main.java.enums.Layout;
 
 /**
  * A Payment Interface.
+ * 
  * @author Lim Yao Xian
  * @version 1.0
  * @since 2022-11-01
@@ -15,14 +17,15 @@ import src.main.java.enums.Layout;
 public class ExceptionHandling {
     /**
      * Integer scanner method with exception handling.
+     * 
      * @return User Integer input.
      */
     public static int IntegerScanner() {
         Scanner scanner = new Scanner(System.in);
-        int num=0;
+        int num = 0;
         try {
             num = scanner.nextInt();
-            //System.out.println("You entered: " + num);
+            // System.out.println("You entered: " + num);
         } catch (InputMismatchException e) {
             System.out.println("Please enter an integer.");
             num = ExceptionHandling.IntegerScanner();
@@ -31,12 +34,17 @@ public class ExceptionHandling {
         return num;
     }
 
-    public static Layout checkLayout(){
+    /**
+     * Layout scanner method with exception handling.
+     * 
+     * @return Layout input.
+     */
+    public static Layout checkLayout() {
         Scanner scanner = new Scanner(System.in);
         Layout layout;
         try {
             layout = Layout.valueOf(scanner.nextLine());
-            //System.out.println("You entered: " + num);
+            // System.out.println("You entered: " + num);
         } catch (IllegalArgumentException e) {
             System.out.println("Please enter one of the following: ");
             System.out.println("small, medium, large");
@@ -45,7 +53,12 @@ public class ExceptionHandling {
         return layout;
     }
 
-    public static ClassOfCinema checkClassOfCinema(){
+    /**
+     * ClassOfCinema scanner method with exception handling.
+     * 
+     * @return ClassOfCinema input.
+     */
+    public static ClassOfCinema checkClassOfCinema() {
         Scanner scanner = new Scanner(System.in);
         ClassOfCinema cinemaClass;
         try {
@@ -60,15 +73,16 @@ public class ExceptionHandling {
 
     /**
      * LocalDateTime scanner method with exception handling.
+     * 
      * @return User LocalDateTime input.
      */
-    public static LocalDateTime checkDateTime(){
+    public static LocalDateTime checkDateTime() {
         Scanner sc = new Scanner(System.in);
         String temp = "";
         LocalDateTime dateTime;
         try {
             temp = sc.nextLine();
-            //convert string to datetime
+            // convert string to datetime
             dateTime = LocalDateTime.parse(temp, _DateTimeFormatter.formatter);
         } catch (Exception e) {
             // TODO: handle exception
@@ -80,39 +94,40 @@ public class ExceptionHandling {
 
     /**
      * String and Character scanner method with exception handling.
+     * 
      * @param option Checker invoked.
      * @return User String input.
      */
-    public static String checkForAlphabet(int option){
-        //2 means expect characters, 1 means expect alphabets
+    public static String checkForAlphabet(int option) {
+        // 2 means expect characters, 1 means expect alphabets
         Scanner scanner = new Scanner(System.in);
         String str = "";
-        
+
         switch (option) {
             case 1:
-            try{
-                //check if str is character
-                str = scanner.nextLine();
-                if(!str.matches("[a-zA-Z]+")){
-                    throw new InputMismatchException();
+                try {
+                    // check if str is character
+                    str = scanner.nextLine();
+                    if (!str.matches("[a-zA-Z]+")) {
+                        throw new InputMismatchException();
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Please enter the correct character.");
+                    str = ExceptionHandling.checkForAlphabet(1);
                 }
-            }catch(InputMismatchException e){
-                System.out.println("Please enter the correct character.");
-                str = ExceptionHandling.checkForAlphabet(1);
-            }
                 break;
-            
+
             case 2:
-            try{
-                //check if str is alphabet
-                str = scanner.nextLine();
-                if(str.matches("[a-zA-Z]+")){
-                    throw new InputMismatchException();
+                try {
+                    // check if str is alphabet
+                    str = scanner.nextLine();
+                    if (str.matches("[a-zA-Z]+")) {
+                        throw new InputMismatchException();
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("You did not enter numerical number.");
+                    str = ExceptionHandling.checkForAlphabet(2);
                 }
-            }catch(InputMismatchException e){
-                System.out.println("You did not enter numerical number.");
-                str = ExceptionHandling.checkForAlphabet(2);
-            }
                 break;
             default:
                 break;
@@ -123,21 +138,22 @@ public class ExceptionHandling {
 
     /**
      * Interger Scanner with bounded valid input.
+     * 
      * @param upperBound Highest valid Integer input.
      * @return User Integer input.
      */
-    public static int IntegerScannerRangeOfFunction(int upperBound){
+    public static int IntegerScannerRangeOfFunction(int upperBound) {
         Scanner scanner = new Scanner(System.in);
-        int num=0;
+        int num = 0;
         try {
             num = scanner.nextInt();
-            //System.out.println("You entered: " + num);
+            // System.out.println("You entered: " + num);
         } catch (InputMismatchException e) {
             System.out.println("You did not enter an integer.");
         }
 
-        while(num<=0 || num>upperBound){
-            System.out.println("Please enter a number between 1 and "+upperBound);
+        while (num <= 0 || num > upperBound) {
+            System.out.println("Please enter a number between 1 and " + upperBound);
             num = scanner.nextInt();
         }
 
@@ -146,19 +162,20 @@ public class ExceptionHandling {
 
     /**
      * String scanner with exception handling.
+     * 
      * @return User String input.
      */
-    public static String StringScanner(){
-	Scanner scanner = new Scanner(System.in);
-	String str="";
-	try {
-	     str = scanner.nextLine();
-             //System.out.println("You typed in: " + str);
-	} catch (InputMismatchException e) {
-         System.out.println("You did not enter a string.");
-    }
+    public static String StringScanner() {
+        Scanner scanner = new Scanner(System.in);
+        String str = "";
+        try {
+            str = scanner.nextLine();
+            // System.out.println("You typed in: " + str);
+        } catch (InputMismatchException e) {
+            System.out.println("You did not enter a string.");
+        }
 
-    return str;
-}
+        return str;
+    }
 
 }
